ae3147e1214d6d049dba9d2d65365cd45acb680f
priyanshibhadja <96171232+priyanshibhadja@users.noreply.github.com>
2022-08-06 22:59:33 +0530

    Add files via upload

---
+++ Pr5.java
@@ -0,0 +1,20 @@
+class Calculate
+{
+  void sum (int a, int b)
+  {
+    System.out.println("sum is"+(a+b)) ;
+  }
+  void sum (float a, float b)
+  {
+    System.out.println("sum is"+(a+b));
+  }
+}
+class Pr5
+{
+      public static void main (String[] args)
+      {
+            Calculate  cal = new Calculate();
+            cal.sum (8,5);      //sum(int a, int b) is method is called.
+            cal.sum (4.6f, 3.8f); //sum(float a, float b) is called.
+  }
+}
